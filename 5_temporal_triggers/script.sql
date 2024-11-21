ccf67e16bd393b0fb50927135eb4939e3aa7f8be
georgekaranja <167513991+geoegekaranja@users.noreply.github.com>
2024-11-21 13:29:43 +0300

    Update script.sql
    A9
    
    Signed-off-by: georgekaranja <167513991+geoegekaranja@users.noreply.github.com>

--- 5_temporal_triggers/script.sql
+++ 5_temporal_triggers/script.sql
@@ -1,1 +1,25 @@
+-- Trigger to update poultry age daily
+DELIMITER //
+CREATE TRIGGER `TRG_UPDATE_POULTRY_AGE` 
+AFTER UPDATE ON `Poultry`
+FOR EACH ROW
+BEGIN
+    UPDATE `Poultry` 
+    SET age = age + 1 
+    WHERE `poultry_id` = OLD.poultry_id;
+END;
+//
+DELIMITER ;
+
+-- Trigger to monitor weekly feed consumption
+DELIMITER //
+CREATE TRIGGER `TRG_MONITOR_FEED_CONSUMPTION` 
+AFTER INSERT ON `FeedConsumption`
+FOR EACH ROW
+BEGIN
+    INSERT INTO `FeedConsumptionStats` (average_feed_per_bird) 
+    VALUES (SELECT AVG(feed_amount) FROM `FeedConsumption` WHERE WEEK(feed_date) = WEEK(CURRENT_DATE));
+END;
+//
+DELIMITER ;
 -- Write your SQL code here
